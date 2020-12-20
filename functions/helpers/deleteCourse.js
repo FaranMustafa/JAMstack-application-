877903b7affa9be78a8899ca5a8eed9d25054843
FaranMustafa <fmustafa044@gmail.com>
2020-12-21 02:07:11 +0500

    delete courses

--- functions/helpers/deleteCourse.js
+++ functions/helpers/deleteCourse.js
@@ -2,12 +2,13 @@ const { table } = require("./airtable");
 const formattedReturn = require("./formattedReturn");
 module.exports = async (event) => {
   // TODO: delete course
-  const { id } = JSON.parse(event.body);
   try {
-    const deletedCourse = await table.destroy(id);
-    return formattedReturn(200, deletedCourse);
+    await fetch("/api/courses", {
+      method: "DELETE",
+      body: JSON.stringify({ id: course.id }),
+    });
+    refreshCourses();
   } catch (err) {
     console.error(err);
-    return formattedReturn(500, {});
   }
 };
