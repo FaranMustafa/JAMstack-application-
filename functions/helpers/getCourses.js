385fa179664251ddcc366d4e83bf504dd380e6e2
FaranMustafa <fmustafa044@gmail.com>
2020-12-20 22:51:16 +0500

    get cources error handling

--- functions/helpers/getCourses.js
+++ functions/helpers/getCourses.js
@@ -3,6 +3,11 @@ const formattedReturn = require("./formattedReturn");
 
 module.exports = async (event) => {
   // TODO: get courses
-  const courses = await table.select().firstPage();
-  return formattedReturn(200, courses);
+  try {
+    const courses = await table.select().firstPage();
+    return formattedReturn(200, courses);
+  } catch (err) {
+    console.error(err);
+    return formattedReturn(500, {});
+  }
 };
