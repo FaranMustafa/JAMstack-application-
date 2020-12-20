5803fc6797badaa35d3220dcf2fff65541b188f3
FaranMustafa <fmustafa044@gmail.com>
2020-12-20 22:49:59 +0500

    getcources helper function

--- functions/helpers/getCourses.js
+++ functions/helpers/getCourses.js
@@ -1,5 +1,8 @@
-const { table } = require('./airtable');
-const formattedReturn = require('./formattedReturn');
+const { table } = require("./airtable");
+const formattedReturn = require("./formattedReturn");
+
 module.exports = async (event) => {
-    // TODO: get courses
+  // TODO: get courses
+  const courses = await table.select().firstPage();
+  return formattedReturn(200, courses);
 };
